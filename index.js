d6a5eb5b8b0063cfa5f6336ad5eefbae33a76680
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-08-11 14:38:51 -0600

    change rpc buttons to images and styles

--- index.js
+++ index.js
@@ -16,9 +16,8 @@ const playRound = (playerSelection) => {
 	) {
 		playerWins++;
 		configureScoreBoard(
-			`You win this round! ${capitalize(playerSelection)} beats ${capitalize(
-				computerSelection
-			)}`
+			'You win this round!',
+			`${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`
 		);
 	} else if (
 		(playerSelection === 'rock' && computerSelection === 'paper') ||
@@ -27,13 +26,13 @@ const playRound = (playerSelection) => {
 	) {
 		computerWins++;
 		configureScoreBoard(
-			`You lose this time... ${capitalize(
-				computerSelection
-			)} beats ${capitalize(playerSelection)}`
+			'You lose this time...',
+			`${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`
 		);
 	} else {
 		configureScoreBoard(
-			`It's a draw... you both played ${capitalize(playerSelection)}`
+			"It's a draw...",
+			`you both played ${capitalize(playerSelection)}`
 		);
 	}
 
@@ -41,11 +40,10 @@ const playRound = (playerSelection) => {
 	if (winner) endGame(winner);
 };
 
-const configureScoreBoard = (messageStr) => {
+const configureScoreBoard = (messageStr, subMessageStr = '') => {
 	score.textContent = `${playerWins} - ${computerWins}`;
 	roundMessage.textContent = messageStr;
-	scoreBoard.style.backgroundColor = 'rgba(0,0,0,0.1)';
-	scoreBoard.style.color = 'black';
+	subMessage.textContent = subMessageStr;
 };
 
 // configures scoreboard for winner
@@ -83,6 +81,8 @@ const reset = () => {
 	continueGame = true;
 	playerWins = 0;
 	computerWins = 0;
+	scoreBoard.style.backgroundColor = 'rgba(0,0,0,0.05)';
+	scoreBoard.style.color = 'black';
 	configureScoreBoard(beginMessage);
 };
 
@@ -90,9 +90,12 @@ let firstToScore = 5;
 let playerWins = 0;
 let computerWins = 0;
 let continueGame = true;
-let beginMessage = `Pick a play to begin! First to ${firstToScore} wins!`;
+let beginMessage = 'Pick a play to begin!';
 
 const container = document.querySelector('.container');
+
+const subTitle = document.querySelector('#sub-title');
+subTitle.textContent = `First to ${firstToScore} wins!`;
 
 const scoreBoard = document.createElement('div');
 scoreBoard.classList.add('score-board');
@@ -101,16 +104,25 @@ scoreBoard.textContent = 'Score';
 const score = document.createElement('div');
 score.classList.add('score');
 
+// create elements for main and sub messages
 const roundMessage = document.createElement('p');
+roundMessage.classList.add('message');
+
+const subMessage = document.createElement('p');
+subMessage.classList.add('sub-message');
+
+// Configure message for scoreboard
 configureScoreBoard(beginMessage);
 
+// append nodes to DOM
 scoreBoard.appendChild(score);
 
-container.appendChild(scoreBoard);
 container.appendChild(roundMessage);
+container.appendChild(subMessage);
+container.appendChild(scoreBoard);
 
 const resetGame = document.createElement('button');
-resetGame.textContent = 'Play Again?';
+resetGame.textContent = 'Play Again';
 resetGame.addEventListener('click', () => {
 	reset();
 });
